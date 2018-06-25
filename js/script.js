6ac75e4aaf0eaefdfe35559c46de7e31207787fe
Artem <gretskovart@gmail.com>
2018-06-25 21:19:36 +0400

    Bags fixing

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function selectMenuPage() {
+  var link = document.getElementById( 'pseudo-menu' ).children;
+  var table = document.getElementsByClassName( 'menu-category' );
+  var arrayLinks = [];
+
+  for (var i = 0; i < link.length; i++) {
+    //Добавляем псевдоссылки в массив
+    let arrValue = link[i].firstChild.innerHTML;
+    arrayLinks.push(arrValue);
+
+    link[i].firstChild.onclick = function changeMenuPage() {
+      let a = this.innerHTML.toString();
+      let index = arrayLinks.indexOf(a);
+
+      document.querySelector( '#pseudo-menu .active' ).classList.remove( 'active' );
+      document.querySelector( '.menu-category.active' ).classList.remove( 'active' );
+
+      this.parentNode.classList.add( 'active' );
+      table[index].classList.add( 'active' );
+
+      return false;
+    };
+  };
+};
+
+selectMenuPage();
